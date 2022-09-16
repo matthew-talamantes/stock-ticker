60e141968871c1c762198c58d460c7d2742e1e13
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-09-16 17:13:25 -0400

    Add basic test of api

--- APITest.py
+++ APITest.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_key():
     with open('./alphavantage_Key.txt', 'r') as keyFile:
@@ -6,3 +7,15 @@ def get_key():
     
     key = key.strip()
     return key
+
+def get_stock_data(symbol, url, key):
+    data = requests.get(f'{url}TIME_SERIES_DAILY&symbol={symbol}&apikey={key}')
+    print(json.loads(data.text))
+
+#main
+url = 'https://www.alphavantage.co/query?function='
+key = get_key()
+symbol = input('Please enter a stock symbol: ')
+get_stock_data(symbol, url, key)
+
+
