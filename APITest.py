4d7abe55dc99b1ed2f2591a034eddae4f0f49678
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-09-16 17:33:09 -0400

    Add way to get active stocks

--- APITest.py
+++ APITest.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 def get_key():
     with open('./alphavantage_Key.txt', 'r') as keyFile:
@@ -12,10 +13,21 @@ def get_stock_data(symbol, url, key):
     data = requests.get(f'{url}TIME_SERIES_DAILY&symbol={symbol}&apikey={key}')
     print(json.loads(data.text))
 
+def get_active_stocks(url, key):
+    apiUrl = f'{url}LISTING_STATUS&state=active&apikey={key}'
+    with requests.Session() as s:
+        download = s.get(apiUrl)
+        decodedContent = download.content.decode('utf-8')
+        cr = csv.reader(decodedContent.splitlines(), delimiter=',')
+        myList = list(cr)
+        for row in myList:
+            print(row)
+
 #main
 url = 'https://www.alphavantage.co/query?function='
 key = get_key()
-symbol = input('Please enter a stock symbol: ')
-get_stock_data(symbol, url, key)
+get_active_stocks(url, key)
+# symbol = input('Please enter a stock symbol: ')
+# get_stock_data(symbol, url, key)
 
 
