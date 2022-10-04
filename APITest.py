1ab95d36ef38a64d1fb1332dd796f3ed891e6772
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-10-03 21:20:27 -0400

    Add write to db functionality

--- APITest.py
+++ APITest.py
@@ -2,6 +2,8 @@ import requests
 import json
 import csv
 
+from dbInterface import StockDatabase
+
 def build_url(url, key, apiFunction, apiParams = None):
     if apiParams == None:
         apiParams = {}
@@ -18,18 +20,25 @@ def get_stock_data(symbol, url, key):
     print(json.loads(data.text))
 
 def get_active_stocks(url, key):
+    """Returns the list of currently active stocks"""
     apiUrl = f'{url}LISTING_STATUS&state=active&apikey={key}'
     with requests.Session() as s:
         download = s.get(apiUrl)
         decodedContent = download.content.decode('utf-8')
         cr = csv.reader(decodedContent.splitlines(), delimiter=',')
         myList = list(cr)
-    return myList
+    return myList[1::]
 
 #main
 url = 'https://www.alphavantage.co/query?function='
 key = get_key()
-get_active_stocks(url, key)
+activeStocks = get_active_stocks(url, key)
+db = StockDatabase('sqlite:///activestocks.db')
+for row in activeStocks:
+    db.add_listing(row[0], row[1], row[2], row[3])
+db.commit()
+db.close()
+print('done')
 # symbol = input('Please enter a stock symbol: ')
 # get_stock_data(symbol, url, key)
 
